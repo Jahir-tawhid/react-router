919857db5681b02ed3a99174d8943c56bdc0e619
jahir-tawhid <jahirtawhid1988@gmail.com>
2026-01-13 16:36:24 +0600

    done

--- src/components/ErrorPage/ErrorPage.jsx
+++ src/components/ErrorPage/ErrorPage.jsx
@@ -1,7 +1,7 @@
 const ErrorPage = () => {
   return (
-    <div>
-      <h1>Opps... so Sorry</h1>
+    <div className="text-red-600 text-4xl font-bold text-center mt-8">
+      <h1>Opps...! so Sorry </h1>
     </div>
   );
 };
